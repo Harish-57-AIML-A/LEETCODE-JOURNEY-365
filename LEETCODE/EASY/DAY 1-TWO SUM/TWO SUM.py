b9386f9073b422ebc346bc2ee9d954653a2e5241
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-08-28 23:11:44 +0530

    Update TWO SUM.py

--- LEETCODE/EASY/DAY 1-TWO SUM/TWO SUM.py
+++ LEETCODE/EASY/DAY 1-TWO SUM/TWO SUM.py
@@ -1,1 +1,9 @@
+def twoSum(numbers, target):
+    num_map = {}  # dictionary to store value -> index
+    for i, x in enumerate(numbers):
+        complement = target - x
+        if complement in num_map:
+            return [num_map[complement] + 1, i + 1]  # +1 for 1-based indexing
+        num_map[x] = i
+    raise ValueError("No two sum solution")
 
