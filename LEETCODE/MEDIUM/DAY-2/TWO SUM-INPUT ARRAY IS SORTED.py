6a8e3d4ec2e7115e3a129712e8a2d072aecbb59c
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-08-29 23:12:43 +0530

    Update TWO SUM-INPUT ARRAY IS SORTED.py

--- LEETCODE/MEDIUM/DAY-2/TWO SUM-INPUT ARRAY IS SORTED.py
+++ LEETCODE/MEDIUM/DAY-2/TWO SUM-INPUT ARRAY IS SORTED.py
@@ -1,1 +1,12 @@
+def twoSum(numbers, target):
+    i, j = 0, len(numbers) - 1
+    while i < j:
+        s = numbers[i] + numbers[j]
+        if s < target:
+            i += 1
+        elif s > target:
+            j -= 1
+        else:
+            return [i + 1, j + 1]
+    raise ValueError("No two sum solution")
 
