35ee70358cd8ff343a864e5c9804a238fec66993
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-08-30 23:05:31 +0530

    Update TWO SUM III.py

--- LEETCODE/EASY/DAY 3/TWO SUM-III/TWO SUM III.py
+++ LEETCODE/EASY/DAY 3/TWO SUM-III/TWO SUM III.py
@@ -1,1 +1,25 @@
+class TwoSum:
+    def __init__(self):
+        self.table = {}
+    
+    def add(self, number: int) -> None:
+        self.table[number] = self.table.get(number, 0) + 1
+    
+    def find(self, value: int) -> bool:
+        for num in self.table:
+            y = value - num
+            if y == num:
+                if self.table[num] >= 2:
+                    return True
+            elif y in self.table:
+                return True
+        return False
+
+# Example Usage
+ts = TwoSum()
+ts.add(1)
+ts.add(3)
+ts.add(5)
+print(ts.find(4))  # True
+print(ts.find(7))  # False
 
