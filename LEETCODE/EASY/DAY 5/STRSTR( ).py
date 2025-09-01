1c247e6066f614d44048ce7323179c58e63e1175
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-09-01 23:06:09 +0530

    Update STRSTR( ).py

--- LEETCODE/EASY/DAY 5/STRSTR( ).py
+++ LEETCODE/EASY/DAY 5/STRSTR( ).py
@@ -1,1 +1,13 @@
+def strStr(haystack: str, needle: str) -> int:
+    for i in range(len(haystack) - len(needle) + 1):
+        for j in range(len(needle)):
+            if haystack[i + j] != needle[j]:
+                break
+        else:
+            return i
+    return -1
+
+# Example Usage
+print(strStr("hello", "ll"))  # 2
+print(strStr("aaaaa", "bba"))  # -1
 
