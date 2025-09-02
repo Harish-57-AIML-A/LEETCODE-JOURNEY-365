84dfd14c5d13dfaedbbab2dd2bdea97c4c3e420e
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-09-02 11:22:14 +0530

    Update TWO SUM III.java

--- LEETCODE/EASY/DAY 3/TWO SUM-III/TWO SUM III.java
+++ LEETCODE/EASY/DAY 3/TWO SUM-III/TWO SUM III.java
@@ -1,1 +1,23 @@
-
+class TwoSum {
+    private Map<Integer, Integer> table = new HashMap<>();
+    
+    // Add number to the data structure
+    public void add(int number) {
+        int count = table.getOrDefault(number, 0);
+        table.put(number, count + 1);
+    }
+    
+    // Find if there exists a pair with given sum
+    public boolean find(int value) {
+        for (Map.Entry<Integer, Integer> entry : table.entrySet()) {
+            int num = entry.getKey();
+            int y = value - num;
+            if (y == num) {
+                if (entry.getValue() >= 2) return true;
+            } else if (table.containsKey(y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
