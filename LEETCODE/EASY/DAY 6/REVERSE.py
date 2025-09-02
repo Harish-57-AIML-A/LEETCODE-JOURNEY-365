e75c43defdfb24acae537414a427767b06727b5a
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-09-02 11:15:37 +0530

    Update REVERSE.py

--- LEETCODE/EASY/DAY 6/REVERSE.py
+++ LEETCODE/EASY/DAY 6/REVERSE.py
@@ -1,1 +1,8 @@
+def reverseWords(s: str) -> str:
+    words = s.strip().split()
+    return " ".join(reversed(words))
+
+# Example Usage
+print(reverseWords("  the sky   is blue "))  # "blue is sky the"
+print(reverseWords("hello world"))           # "world hello"
 
