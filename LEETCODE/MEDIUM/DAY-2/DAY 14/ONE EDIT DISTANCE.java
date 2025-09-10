43cc7bd641b63d88a2eef2c52152435d7d968bba
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-09-10 21:13:44 +0530

    Update ONE EDIT DISTANCE.java

--- LEETCODE/MEDIUM/DAY-2/DAY 14/ONE EDIT DISTANCE.java
+++ LEETCODE/MEDIUM/DAY-2/DAY 14/ONE EDIT DISTANCE.java
@@ -1,1 +1,26 @@
+class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        int m = s.length(), n = t.length();
+        if (m > n) return isOneEditDistance(t, s);
+        if (n - m > 1) return false;
+
+        int i = 0;
+        while (i < m && s.charAt(i) == t.charAt(i)) i++;
+
+        if (i == m) return n - m == 1; // Extra char at end
+
+        if (m == n) i++; // Modification case
+
+        while (i < m && s.charAt(i) == t.charAt(i + (n - m))) i++;
+
+        return i == m;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.isOneEditDistance("ab", "acb"));   // true
+        System.out.println(sol.isOneEditDistance("cab", "ad"));   // false
+        System.out.println(sol.isOneEditDistance("1203", "1213")); // true
+    }
+}
 
