b88564d2c8b616018693f69047100c81e449e2c7
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-09-12 22:08:39 +0530

    Update READ N CHAR GIVEN READ4 - MULTIPLE CALLS.py

--- LEETCODE/HARD/DAY 11/DAY 16/READ N CHAR GIVEN READ4 - MULTIPLE CALLS.py
+++ LEETCODE/HARD/DAY 11/DAY 16/READ N CHAR GIVEN READ4 - MULTIPLE CALLS.py
@@ -1,1 +1,36 @@
+# Simulated API
+def read4(buf4):
+    global file_data, file_ptr
+    count = 0
+    while count < 4 and file_ptr < len(file_data):
+        buf4[count] = file_data[file_ptr]
+        file_ptr += 1
+        count += 1
+    return count
+
+class Reader:
+    def __init__(self):
+        self.buffer = [""] * 4
+        self.offset = 0
+        self.bufsize = 0
+
+    def read(self, buf, n):
+        readBytes = 0
+        eof = False
+
+        while not eof and readBytes < n:
+            sz = self.bufsize if self.bufsize > 0 else read4(self.buffer)
+
+            if self.bufsize == 0 and sz < 4:
+                eof = True
+
+            bytes_to_copy = min(n - readBytes, sz)
+            for i in range(bytes_to_copy):
+                buf.append(self.buffer[self.offset + i])
+
+            self.offset = (self.offset + bytes_to_copy) % 4
+            self.bufsize = sz - bytes_to_copy
+            readBytes += bytes_to_copy
+
+        return readBytes
 
