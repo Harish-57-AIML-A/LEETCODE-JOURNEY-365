d4d4a60bc46cdc77d1d9821b272d795b124c98b6
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-09-30 16:17:53 +0530

    Create BINARY TREE UPSIDE DOWN.java

---
+++ LEETCODE/MEDIUM/DAY 31/BINARY TREE UPSIDE DOWN.java
@@ -0,0 +1,11 @@
+public TreeNode UpsideDownBinaryTree(TreeNode root) {
+    return dfsBottomUp(root, null);
+}
+
+private TreeNode dfsBottomUp(TreeNode p, TreeNode parent) {
+    if (p == null) return parent;
+    TreeNode root = dfsBottomUp(p.left, p);
+    p.left = (parent == null) ? null : parent.right;
+    p.right = parent;
+    return root;
+}
