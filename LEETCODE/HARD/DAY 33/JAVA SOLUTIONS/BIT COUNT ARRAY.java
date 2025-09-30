714984a9cc47b0d8af532c7f724a29a6f1da61c0
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-09-30 16:55:15 +0530

    Create BIT COUNT ARRAY.java

---
+++ LEETCODE/HARD/DAY 33/JAVA SOLUTIONS/BIT COUNT ARRAY.java
@@ -0,0 +1,11 @@
+public int singleNumber(int[] nums) {
+    int[] count = new int[32];
+    int result = 0;
+    for (int i = 0; i < 32; i++) {
+        for (int num : nums) {
+            if (((num >> i) & 1) == 1) count[i]++;
+        }
+        result |= (count[i] % 3) << i;
+    }
+    return result;
+}
