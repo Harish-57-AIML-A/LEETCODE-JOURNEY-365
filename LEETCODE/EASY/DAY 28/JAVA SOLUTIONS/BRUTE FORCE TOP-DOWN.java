dc5ecc4c0bc492fea4ac131ca7bbd3ce7b826775
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-09-30 16:00:40 +0530

    Create BRUTE FORCE TOP-DOWN.java

---
+++ LEETCODE/EASY/DAY 28/JAVA SOLUTIONS/BRUTE FORCE TOP-DOWN.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1
+               && isBalanced(root.left)
+               && isBalanced(root.right);
+    }
+
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
